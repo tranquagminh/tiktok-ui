4a4356f82cedc29de3b20b47f595ef83d82f09e5
Minh162002 <tranquagminh1720.com>
2023-09-29 13:21:53 +0700

    Build a default freamwork Layout

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,14 @@
+import style from './SideBar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(style);
+
 function SideBar() {
-    return <h1>SideBar</h1>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>SideBar pages</h2>
+        </aside>
+    );
 }
 
 export default SideBar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import style from './Header.module.scss';
+
+const cx = classNames.bind(style);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
